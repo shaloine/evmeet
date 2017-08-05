6758c3a12745cf934e6cea6aa2b93d5f3b854341
shaloine <shaloine2@hotmail.com>
2017-08-05 13:51:52 +0200

    mise en place entites

---
+++ src/PW/EvmeetBundle/Entity/Comment.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace PW\EvmeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ *
+ * @ORM\Table(name="comment")
+ * @ORM\Entity(repositoryClass="PW\EvmeetBundle\Repository\CommentRepository")
+ */
+class Comment
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var text
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="PW\EvmeetBundle\Entity\User", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+
+    public function __construct()
+    {
+      
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Comment
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \PW\EvmeetBundle\Entity\User $user
+     *
+     * @return Comment
+     */
+    public function setUser(\PW\EvmeetBundle\Entity\User $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \PW\EvmeetBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
