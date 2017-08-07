441ca615b1e611c9be72ff57678b0acb6f4eab89
shaloine <shaloine2@hotmail.com>
2017-08-07 19:04:34 +0200

    modif formulaire + ajout date picker

--- src/PW/EvmeetBundle/Form/ArticleType.php
+++ src/PW/EvmeetBundle/Form/ArticleType.php
@@ -19,9 +19,9 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('lieu', ChoiceType::class, array('choices' => array('EV2' => 1, 'EV3' => 2, 'Labo' => 3)))
-            ->add('dateInvitation', DateTimeType::class)
-            ->add('niveauMin', ChoiceType::class, array('choices' => array('EV2' => 1, 'EV3' => 2, 'Labo' => 3)))
-            ->add('niveauMax', ChoiceType::class, array('choices' => array('EV2' => 1, 'EV3' => 2, 'Labo' => 3)))
+            ->add('dateInvitation', DateTimeType::class, array('widget' => 'single_text','html5' => false))
+            ->add('niveauMin', ChoiceType::class, array('choices' => array('4A' => 1, '4B' => 2, '4C' => 3,'5A' => 4, '5B' => 5, '5C' => 6,'6A' => 7, '6B' => 8, '6C' => 9,'7A' => 10, '7B' => 11, '7C' => 12, '8A' => 13)))
+            ->add('niveauMax', ChoiceType::class, array('choices' => array('4A' => 1, '4B' => 2, '4C' => 3,'5A' => 4, '5B' => 5, '5C' => 6,'6A' => 7, '6B' => 8, '6C' => 9,'7A' => 10, '7B' => 11, '7C' => 12, '8A' => 13)))
             ->add('commentaire', TextareaType::class)
             ->add('save', SubmitType::class);
     }
