89f38d21101452f1fcfa10663b7595a21622d37d
shaloine <shaloine2@hotmail.com>
2017-08-27 12:15:17 +0200

    filtertype

--- src/PW/EvmeetBundle/Form/FilterType.php
+++ src/PW/EvmeetBundle/Form/FilterType.php
@@ -17,9 +17,9 @@ class FilterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lieu', ChoiceType::class, array('choices' => array('EV2' => 'EV2', 'EV3' => 'EV3', 'Labo' => 'Labo')))
-            ->add('dateInvitation', DateType::class, array('widget' => 'single_text','html5' => false, 'format' => 'dd/MM/yyyy'))
-            ->add('niveau', ChoiceType::class, array('choices' => array('4A' => 1, '4B' => 2, '4C' => 3,'5A' => 4, '5B' => 5, '5C' => 6,'6A' => 7, '6B' => 8, '6C' => 9,'7A' => 10, '7B' => 11, '7C' => 12, '8A' => 13)))
+            ->add('lieu', ChoiceType::class, array('choices' => array('EV2' => 'EV2', 'EV3' => 'EV3', 'Labo' => 'Labo'), 'placeholder' => 'Choisir un lieu','required' => false))
+            ->add('dateInvitation', DateType::class, array('widget' => 'single_text','html5' => false, 'format' => 'dd/MM/yyyy','required' => false))
+            ->add('niveau', ChoiceType::class, array('choices' => array('4A' => 1, '4B' => 2, '4C' => 3,'5A' => 4, '5B' => 5, '5C' => 6,'6A' => 7, '6B' => 8, '6C' => 9,'7A' => 10, '7B' => 11, '7C' => 12, '8A' => 13),'required' => false, 'placeholder' => 'Choisir un niveau'))
             ->add('save', SubmitType::class, array('label' => 'Filtrer'));
     }
     
